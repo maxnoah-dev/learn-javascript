d7acb1c71defb792c8564b0293a97d02921d555f
maxnoah-dev <hsondz1910@gmail.com>
2025-10-11 23:50:10 +0700

    Revamp index.html to create a user registration form and update index.js to handle registration functionality

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-let btnCount = document.getElementById("btn-count");
-let textAppear = document.getElementById("countNum");
-let count = 0;
-let countv2 = 0;
+// let btnCount = document.getElementById("btn-count");
+// let textAppear = document.getElementById("countNum");
+// let count = 0;
+// let countv2 = 0;
 
-function CountNumber() {
-    count += 1;
-    textAppear.innerHTML = count
-    console.log(count);
+// function CountNumber() {
+//     count += 1;
+//     textAppear.innerHTML = count
+//     console.log(count);
+// }
+
+// const CountNumberv2 = () => {
+//     countv2 += 1;
+//     textAppear.innerHTML = count
+//     console.log(count);
+// }
+
+// btnCount.addEventListener('click', CountNumberv2);
+
+let username = document.getElementById("username");
+let password = document.getElementById("pwd");
+let male = document.getElementById("male");
+let fmale = document.getElementById("female");
+let btnRegister = document.getElementById("btn-register");
+
+function registerAccount() {
+    
 }
 
-const CountNumberv2 = () => {
-    countv2 += 1;
-    textAppear.innerHTML = count
-    console.log(count);
-}
-
-btnCount.addEventListener('click', CountNumberv2);
+btnRegister.addEventListener('click', registerAccount);
